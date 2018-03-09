119d154f03d288fd2865182ef73e7e62c55595c7
贾元发 <jiayuanfa@yeah.net>
2018-03-09 10:31:38 +0800

    RN生命周期演示

--- App.js
+++ App.js
@@ -2,6 +2,10 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  * @flow
+ * RN的生命周期：
+ * 1：RN的UI相应
+ * 2：RN的数据刷新
+ * 3：ES5、ES6的区别
  */
 
 import React, { Component } from 'react';
@@ -14,18 +18,14 @@ import {
   Alert
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-// corporate ipsum > lorem ipsum
-var alertMessage = 'Credibly reintermediate next-generation potentialities after goal-oriented ' +
-                   'catalysts for change. Dynamically revolutionize.';
-
 type Props = {};
+
+/**
+* Props 属性 相当于OC中的ReadOnly，只读属性！！
+* state 状态 可以改变的 每个组件有一个SetState方法 用来改变状态 而且刷新界面！！
+* componentWillMount 相当于OC中的ViewWillAppear在组件即将加载或者显示的时候调用！！
+* RN默认的周期内Alert内只会来两次，但是我们通过事件主动去调用Alert，就可以弹出多次了
+*/
 
 // 给View添加点击能力，并监听其点击事件
 export default class App extends Component<Props> {
@@ -35,30 +35,87 @@ export default class App extends Component<Props> {
     title:'默认值'
   }
 
+// ViewWillAppear 视图即将显示的时候
+  componentWillMount(){
+    // Alert.alert('componentWillMount')
+  }
+
+// 刷新完UI之后就来了 相当于iOS的ViewDidLoad，这个函数今后用来发送网络请求（第一次加载的数据）
+  componentDidMount(){
+        // Alert.alert('componentDidMount')
+  }
+
+// 视图点击后数据刷新UI成功之后将会回调 第一次加载UI的时候不会来
+  componentDidUpdate(){
+    Alert.alert('componentDidUpdate')
+  }
+
+// 是一个异步函数 也就是输如果快速多次改变state的值，并不会很快的刷新
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity activeOpacity={0.3} onPress={()=>this.click('点击')}
 
-                                              onPressIn={()=>this.click('按下')}
-                                              onPressOut={()=>this.click('抬起')}
-                                              onLongPress={()=>this.click('长按')}>
-        <View>
+      <TouchableOpacity onPress={()=>this.click('点击')}>
+      {Alert.alert('render 来了')}
         <Text>{this.state.title}</Text>
-        </View>
-        </TouchableOpacity>
+      </TouchableOpacity>
+
       </View>
     );
   }
 
-// 方法必须写在export中才行
   click(event){
     this.setState({
       title:event
     })
-   }
+  }
 
 }
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//     'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+
+// // corporate ipsum > lorem ipsum
+var alertMessage = 'Credibly reintermediate next-generation potentialities after goal-oriented ' +
+                   'catalysts for change. Dynamically revolutionize.';
+
+
+// // 给View添加点击能力，并监听其点击事件
+// export default class App extends Component<Props> {
+
+// // 状态机 
+//   state={
+//     title:'默认值'
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <TouchableOpacity activeOpacity={0.3} onPress={()=>this.click('点击')}
+
+//                                               onPressIn={()=>this.click('按下')}
+//                                               onPressOut={()=>this.click('抬起')}
+//                                               onLongPress={()=>this.click('长按')}>
+//         <View>
+//         <Text>{this.state.title}</Text>
+//         </View>
+//         </TouchableOpacity>
+//       </View>
+//     );
+//   }
+
+// // 方法必须写在export中才行
+//   click(event){
+//     this.setState({
+//       title:event
+//     })
+//    }
+
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -66,15 +123,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
